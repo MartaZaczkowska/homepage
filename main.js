cebc73ec588c00b6905ac067af075de7218ceddf
Marta Żaczkowska <marta.zaczkowska@gmail.com>
2019-06-12 13:21:51 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log('Witaj!');
+const name = 'Marta';
+const age = 32;
+
+console.log(name);
+console.log(age);
+
+console.log(`Nazywam się ${name} i mam ${age} lat(a). Zapraszam na moją stronę.`);
