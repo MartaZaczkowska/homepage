fa4b61f9c0e257355fa484657228f34c8f93e666
Marta Żaczkowska <marta.zaczkowska@gmail.com>
2019-07-14 19:49:08 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ const welcome = (name, age) => {
 
 welcome(`Artur`, 31)
 
+const navigationSwitcher = document.querySelector(`.navigation__switcher--js`)
+
+navigationSwitcher.addEventListener(`click`, (e) => {
+    const navigationList = document.querySelector(`.navigation__list--js`);
+    navigationList.classList.toggle(`navigation__list--visible`);
+
+});
